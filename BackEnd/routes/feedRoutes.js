278f53c58158c25ba5b7abd0c2afc49c6961fed1
Parveen Kumar <155838628+Parveen020@users.noreply.github.com>
2025-01-26 19:27:24 +0530

    Update feedRoutes.js

--- BackEnd/routes/feedRoutes.js
+++ BackEnd/routes/feedRoutes.js
@@ -9,7 +9,7 @@ const feedRouter = express.Router();
 const storage = multer.diskStorage({
   destination: "uploads",
   filename: (req, file, cb) => {
-    return cb(null, `${Date.now()}${file.originalnamme}`);
+    return cb(null, `${Date.now()}${file.originalname}`);
   },
 });
 
